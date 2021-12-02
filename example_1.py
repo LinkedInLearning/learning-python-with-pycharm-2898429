cf4c29a5bd3ee78f906b8f234f40a334ce0d1f40
LL_Mehdi <oulmakki:@gmail.com>
2021-12-02 12:14:17 +0100

    wrote some horrendous code to demo some aspects of readability

---
+++ example_1.py
@@ -0,0 +1,43 @@
+import pygame
+
+WINDOW_WIDTH = 1200
+WINDOW_HEIGHT = 600
+RECTANGLE_HEIGHT = WINDOW_HEIGHT / 3
+
+ARMENIA_FLAG_COLORS = ('#D90012', '#0033A0', '#F2A800')
+BULGARIA_FLAG_COLORS = ('#FFFFFF', '#009B74', '#D01C1F')
+YEMEN_FLAG_COLORS = ('#CE1126', '#FFFFFF', '#000000')
+
+top_rectangle = pygame.Rect((0, 0), (WINDOW_WIDTH, RECTANGLE_HEIGHT))
+mid_rectangle = pygame.Rect((0, RECTANGLE_HEIGHT), (WINDOW_WIDTH, RECTANGLE_HEIGHT))
+bottom_rectangle = pygame.Rect((0, 2*RECTANGLE_HEIGHT), (WINDOW_WIDTH, RECTANGLE_HEIGHT))
+
+# increment by one every time we draw the animation. Based on the number, we choose which flag to display
+counter = 0
+
+# pygame set up.
+pygame.init()
+pygame.display.set_caption('flags animation')
+window_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+clock = pygame.time.Clock()
+# How many seconds to wait before updating the screen
+update_rate = 1
+
+while True:
+    time_delta = clock.tick(update_rate)
+
+    if counter % 3 == 0:
+        top_color, mid_color, bottom_color = ARMENIA_FLAG_COLORS
+    if counter % 3 == 1:
+        top_color, mid_color, bottom_color = BULGARIA_FLAG_COLORS
+    if counter % 3 == 2:
+        top_color, mid_color, bottom_color = YEMEN_FLAG_COLORS
+
+    pygame.draw.rect(window_surface, top_color, top_rectangle)
+    pygame.draw.rect(window_surface, mid_color, mid_rectangle)
+    pygame.draw.rect(window_surface, bottom_color, bottom_rectangle)
+
+    counter += 1
+    pygame.display.update()
+
+
---
+++ example_2.py
@@ -0,0 +1,35 @@
+a1 = '#D90012'
+b1 = '#0033A0'
+c1 = '#F2A800'
+a2 = '#FFFFFF'
+b2 = '#009B74'
+c2 = '#D01C1F'
+a3 = '#CE1126'
+b3 = '#FFFFFF'
+c3 = '#000000'
+import pygame
+
+x = pygame.Rect((0, 0), (600, 300))
+pygame.init()
+y = pygame.Rect((0, 100), (600, 300))
+
+window_surface = pygame.display.set_mode((1200, 600))
+z = pygame.Rect((0, 200), (600, 300))
+
+token = 0
+
+
+while True:
+    time_delta = pygame.time.Clock().tick(1)
+    if token % 3 == 0: a,b,c=a1,b1,c1
+    if token % 3 == 1: a, b, c = a2, b2, c2
+    if token % 3 == 2: a,b, c= a3,b3, c3
+
+    pygame.draw.rect(window_surface,a, x)
+    pygame.draw.rect(window_surface, b,y)
+    pygame.draw.rect(window_surface,c,z)
+
+    token = token + 1
+    pygame.display.update()
+
+
